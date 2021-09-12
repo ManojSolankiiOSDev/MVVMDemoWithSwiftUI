6d947e219c38020fe8f0c2492c15aa51dc4fd142
Manoj Solanki <manoj.solanki@mindruby.com>
2021-09-12 09:37:19 +0530

    Initial Commit

---
+++ MVVM_SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM_SwiftUI
+//
+//  Created by MANOJ SOLANKI on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
